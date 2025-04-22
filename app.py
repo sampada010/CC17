e3aa5f7f9ff4a595e3f87f3c00721f204366da49
Sampada Tagalpallewar <tagalpallewarsampada10@gmail.com>
2025-04-22 23:36:01 +0530

    changes in app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template_string
+import os
 
 app = Flask(__name__)
 
@@ -30,4 +31,5 @@ def greet():
     return render_template_string(html, name=name)
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
